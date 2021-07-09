5db7bd85db01749390228b02f71c8edbe43bac65
ZJP <563902816@qq.com>
2021-07-09 14:04:47 +0800

    第一天跟着老师敲的信号与槽之类的内容

--- Class_Test_Project/mywindow.cpp
+++ Class_Test_Project/mywindow.cpp
@@ -1,14 +1,43 @@
 #include "mywindow.h"
 #include "ui_mywindow.h"
+#include <QMouseEvent>
 
 MyWindow::MyWindow(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::MyWindow)
 {
     ui->setupUi(this);
+    connect(ui->pushButton,
+            &QPushButton::clicked,
+            this,
+            &MyWindow::slotsTest1);
 }
 
 MyWindow::~MyWindow()
 {
     delete ui;
+}
+
+void MyWindow::on_pushButton_clicked(){
+    this->setWindowTitle("Fuck");
+}
+
+void MyWindow::slotsTest1(){
+
+}
+
+void MyWindow::slotsTest2(){
+    this->setWindowTitle("Hello");
+}
+
+void MyWindow::mousePressEvent(QMouseEvent *e){
+    isPress = true;
+    oldPos = e->pos();
+}
+void MyWindow::mouseReleaseEvent(QMouseEvent *){
+}
+void MyWindow::mouseMoveEvent(QMouseEvent *e){
+    if(!isPress)
+        return;
+   this->move(e->pos() - oldPos + this->pos());
 }
--- Class_Test_Project/mywindow.h
+++ Class_Test_Project/mywindow.h
@@ -15,8 +15,21 @@ public:
     explicit MyWindow(QWidget *parent = nullptr);
     ~MyWindow();
 
+private slots:
+    void on_pushButton_clicked();
+    void slotsTest1();
+    void slotsTest2();
+
+
 private:
+    void mousePressEvent(QMouseEvent *);
+    void mouseReleaseEvent(QMouseEvent *);
+    void mouseMoveEvent(QMouseEvent *);
+
     Ui::MyWindow *ui;
+
+    QPoint oldPos;
+    bool isPress;
 };
 
 #endif // MYWINDOW_H
